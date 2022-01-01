62047e4f45dd25e4fa1e5267843322e2606b57e8
Ujjwal Khatri <ujjwal.khatri@gmail.com>
2022-01-01 20:57:23 +0000

    initial commit

---
+++ src/main/java/ujkhatri/SpringBootTutorial/SpringBootTutorialApplication.java
@@ -0,0 +1,13 @@
+package ujkhatri.SpringBootTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialApplication.class, args);
+	}
+
+}
