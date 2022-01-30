301f7381ca938c1c59db0392cf5e4afca32ffe45
Ujjwal Khatri <ujjwal.khatri@gmail.com>
2022-01-30 17:24:23 +0000

    POST API implementation and RestTempate example

--- src/main/java/ujkhatri/controller/Controller.java
+++ src/main/java/ujkhatri/controller/Controller.java
@@ -1,9 +1,11 @@
 package ujkhatri.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import ujkhatri.Utilities.AsynchronousTasks;
 import ujkhatri.Utilities.CachedData;
 import ujkhatri.model.User;
@@ -21,6 +23,9 @@ public class Controller {
     @Autowired
     CachedData cachedData;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping(value = "/sample")
     public Map getAPI() {
         asynchronousTasks.asynchronousTask();
@@ -34,7 +39,29 @@ public class Controller {
         return successResponse();
     }
 
-    private Map successResponse () {
+    @PostMapping(value = "/add-user", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Map> addUser(
+            @RequestHeader(name = "APPLICATION-LOCATION", defaultValue = "EUROPE") String location,
+            @RequestBody User user) {
+        System.out.println(user.toString());
+        System.out.println(location);
+        return new ResponseEntity<Map>(successResponse(), HttpStatus.OK);
+    }
+
+    //SERVICE_URL defined in application.properties file
+    @Value( "${SERVICE_URL}" )
+    private String serviceURL;
+
+    @GetMapping(value = "/rest-client-demo")
+    public ResponseEntity<Map> restClientDemo() {
+        User user = new User(1, "Suresh");
+        ResponseEntity<Map> response = restTemplate.postForEntity(serviceURL, user, Map.class);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody());
+        return new ResponseEntity<Map>(successResponse(), HttpStatus.OK);
+    }
+
+    private Map successResponse() {
         Map<String, Object> responseMap = new HashMap<>();
         responseMap.put("statusMessage", "success");
         responseMap.put("statusCode", 200);
--- src/main/java/ujkhatri/springboot_tutorial/SpringBootTutorialApplication.java
+++ src/main/java/ujkhatri/springboot_tutorial/SpringBootTutorialApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -15,5 +17,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class SpringBootTutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
